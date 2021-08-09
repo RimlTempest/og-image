3899a4085397b56c0fdf1bd82d9f0c7a246aea26
RimlTempest <riml.slime@gmail.com>
2021-08-10 06:46:38 +0900

    update: image maxlength

--- web/index.ts
+++ web/index.ts
@@ -392,6 +392,11 @@ const App = (_: any, state: AppState, setState: SetState) => {
                             const nextImage = images.length === 1
                                 ? 'https://cdn.jsdelivr.net/gh/remojansen/logo.ts@master/ts.svg'
                                 : '';
+                            if (images.length < 4){
+                                setState({ showToast: true, messageToast: '画像は上限4枚までです。' });
+                                setTimeout(() => setState({ showToast: false }), 2000)
+                                return;
+                            }
                             setLoadingState({ images: [...images, nextImage] })
                         }
                     }),
